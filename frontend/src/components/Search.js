9de161d0d4d56fdc8254e240eb91d727b4604dbd
Nick Costa <costa@Nicks-MacBook-Air.local>
2024-02-22 17:03:18 -0500

    made changes to areas page, switch the areas cards to links of pages of the specific city, with fact cards and links to pages about the cities, will do that for the rest of the cities, added icons

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -5,7 +5,6 @@ const Search = () => {
     return (
         <div className='searchContainer'>
             <div id="MBBv3_LoginPanel"></div>
-            <h1>Search</h1>
             <div id="MBBv3_QuickSearch" filter="formType:simple1"></div>
         </div>
     );
