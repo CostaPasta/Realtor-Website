2856c41de130e206759475a75de685269d709720
Nick Costa <costa@Nicks-MacBook-Air.local>
2024-02-29 14:39:21 -0500

    added shrinking functionality to header when scrolled, fixed the scroll function being triggered repeatedly, however it is still being triggered numerous times, but no longer triggered every milisecond, fixed some header css

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {debounce} from 'lodash';
 import { Link } from 'react-scroll';
 import '../components-css/Header.css';
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
 
   const toggleMenu = () => {
     if (window.innerWidth <= 768) {
@@ -11,34 +14,83 @@ function Header() {
     }
   };
 
+
   // Function to close the menu when screen width is greater than 768px
   const closeMenuOnResize = () => {
     if (window.innerWidth > 768 && menuOpen) {
       setMenuOpen(false);
     }
   };
+
+
   // Add an event listener for window resize
   window.addEventListener('resize', closeMenuOnResize);
+
+  
+  
+
+
+  useEffect(() => {
+    const handleScroll = debounce(() => {
+      const position = window.pageYOffset;
+      setIsScrolled(position > 75);
+  
+      console.log("ITS WORKING!!!")
+    }, 10);
+
+    
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      handleScroll.cancel();
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+  const handleNavLinkClick = (targetId) => {
+    if (window.innerWidth <= 768 && menuOpen) {
+      toggleMenu();
+    }
+    
+
+    setTimeout(() => {
+      const headerHeight = document.querySelector('.header').offsetHeight;
+      //const headerHeightShrink = document.querySelector('.header.shrink').offsetHeight;
+      const targetElement = document.getElementById(targetId);
+  
+      if (targetElement) {
+        const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+        const offsetPosition = targetPosition - headerHeight;
+  
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth',
+        });
+      }
+    }, menuOpen ? 300 : 0);
+
+  };
 
   return (
     <div className='entire-header'>
       <div id="MBBv3_LoginPanel" className="login-panel"></div>
-      <header className={`header ${menuOpen ? 'open' : ''}`}>
+      <header className={`header ${menuOpen ? 'open' : ''} ${isScrolled ? 'shrink' : ''}`}>
         {/* This is the div that visually appears on top of the header */}
        
         <div className="logo">
-          <Link to="SlideShowComponent" smooth={true} duration={500} onClick={toggleMenu}>
+          <Link to="SlideShowComponent" smooth={true} duration={500} onClick={handleNavLinkClick}>
             JOSE COSTA
           </Link>
         </div>
         <nav className={`nav ${menuOpen ? 'open' : ''}`}>
-          <Link to="aboutMeComponent" smooth={true} duration={500} onClick={toggleMenu}>
+          <Link to="aboutMeComponent" smooth={true} duration={500} onClick={handleNavLinkClick}>
             ABOUT ME
           </Link>
-          <Link to="searchComponent" smooth={true} duration={500} onClick={toggleMenu}>
+          <Link to="searchComponent" smooth={true} duration={500} onClick={handleNavLinkClick}>
             SEARCH
           </Link>
-          <Link to="listingsComponent" smooth={true} duration={500} onClick={toggleMenu}>
+          <Link to="listingsComponent" smooth={true} duration={500} onClick={handleNavLinkClick}>
             LISTINGS
           </Link>
           <a href="/contact" onClick={toggleMenu}>CONTACT</a>
