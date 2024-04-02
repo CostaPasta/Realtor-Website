d65c9686a8bba5ecdfe3363f57252e814eec6759
costapasta <nicholasmcosta@gmail.com>
2024-04-02 12:34:14 -0400

    fixed logo link to reintroduce the header rather than making it disappear, also fixed offset of the nav link so it will now navigate to the correct portion

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,9 +18,20 @@ function Header() {
 
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
+    const threshold = 450; // Define your threshold value here
 
     if (!navClicked) {
       setShowHeader(currentScrollY < lastScrollY.current);
+    }
+
+    // Logic to handle header visibility when not within the threshold
+    if (currentScrollY > threshold) {
+      if (!navClicked) {
+        setShowHeader(currentScrollY < lastScrollY.current);
+      }
+    } else {
+      // When within the threshold, allow the header to reappear if scrolling up
+      setShowHeader(true);
     }
 
     lastScrollY.current = currentScrollY;
@@ -40,6 +51,7 @@ function Header() {
   const handleNavClick = () => {
     setNavClicked(true);
     setShowHeader(false);
+    
 
     if (navClickTimeout.current) {
       clearTimeout(navClickTimeout.current); // Clear any existing timeout
@@ -48,7 +60,16 @@ function Header() {
     navClickTimeout.current = setTimeout(() => {
       setNavClicked(false);
     }, 1000); // Adjust the timeout to match your scrolling duration
+
   };
+
+  const handleSpecialNavClick = () => {
+    setShowHeader(true); // Force the header to reappear
+    if (window.innerWidth <= 768) {
+      toggleMenu(); // Also toggle the menu if in mobile view
+    }
+  };
+  
 
   return (
     <ScrollProvider>
@@ -56,7 +77,7 @@ function Header() {
         <div id="MBBv3_LoginPanel" className="login-panel"></div>
         <header className={`header ${menuOpen ? 'open' : ''} ${showHeader ? '' : 'hidden'}`}>
           <div className="logo">
-            <Link to="SlideShowComponent" smooth={true} duration={600} offset={'SlideShowComponent'} onClick={handleNavClick}>
+            <Link to="SlideShowComponent" smooth={true} duration={600} offset={-90} onClick={handleSpecialNavClick}>
               JOSE COSTA
             </Link>
           </div>
