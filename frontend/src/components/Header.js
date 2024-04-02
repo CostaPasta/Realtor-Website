54843d65e53805253a86a5d6f2b1613eb77e2114
costapasta <nicholasmcosta@gmail.com>
2024-04-02 12:34:13 -0400

    added hamburger menu for header when the window is resized, fixed a bug where the menu remains open after user opens the menu then enlarges the window

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import { Link, animateScroll as scroll } from "react-scroll";
+import React, { useState } from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
 import '../components-css/Header.css';
 
-
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Function to close the menu when screen width is greater than 768px
+  const closeMenuOnResize = () => {
+    if (window.innerWidth > 768 && isOpen) {
+      setIsOpen(false);
+    }
+  };
+  // Add an event listener for window resize
+  window.addEventListener('resize', closeMenuOnResize);
+
   return (
-    <header className="header">
+    <header className={`header ${isOpen ? 'open' : ''}`}>
       <div className="logo">JOSE COSTA</div>
-      <nav className="nav">
-        <Link to="searchComponent" smooth={true} duration={500}>
+      <nav className={`nav ${isOpen ? 'open' : ''}`}>
+        <Link to="searchComponent" smooth={true} duration={500} onClick={toggleMenu}>
           SEARCH
         </Link>
-        <Link to="aboutMeComponent" smooth={true} duration={500}>
+        <Link to="aboutMeComponent" smooth={true} duration={500} onClick={toggleMenu}>
           ABOUT ME
         </Link>
-        <Link to="listingsComponent" smooth={true} duration={500}>
+        <Link to="listingsComponent" smooth={true} duration={500} onClick={toggleMenu}>
           LISTINGS
         </Link>
-        <a href="/contact">CONTACT</a>
+        <a href="/contact" onClick={toggleMenu}>CONTACT</a>
       </nav>
+      <button className="menu-icon" onClick={toggleMenu}>
+        <div className={`bar ${isOpen ? 'open' : ''}`} />
+        <div className={`bar ${isOpen ? 'open' : ''}`} />
+        <div className={`bar ${isOpen ? 'open' : ''}`} />
+      </button>
     </header>
   );
 }
