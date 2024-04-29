b88659d0eb16f7ab1ac9e2226794b765a51214a3
costapasta <nicholasmcosta@gmail.com>
2024-04-29 19:44:35 -0400

    made some changes to contact

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BackHeader from './BackHeader';
 
 function Contact() {
   return (
     <div>
-      <div id="MBBv3_Brokers" filter="mappin:blue"></div>
+      <BackHeader/>
+      <div id="MBBv3_Brokers" filter="mappin:blue" style={{marginTop: 30}}></div>
     </div>
   );
 }
