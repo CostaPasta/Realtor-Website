9ed688e7abdea4aca4c7bc74874b356ae299f8c4
costapasta <nicholasmcosta@gmail.com>
2024-05-06 16:36:01 -0400

    test

--- frontend/src/components/Listings.js
+++ frontend/src/components/Listings.js
@@ -5,7 +5,7 @@ const Listings = () => {
     return (
       <div className="listings-wrapper">
         <h1 className='listing-title'>LISTINGS</h1>
-        <div id="MBBv3_FeaturedGallery" filter="agent_id:276518619+listing_status:active"></div>  
+        {/* <div id="MBBv3_FeaturedGallery" filter="agent_id:276518619+listing_status:active"></div>   */}
       </div>
   );
   
