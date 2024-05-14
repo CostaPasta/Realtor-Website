5108b615792c3310fd1706ba59545bb0bff61a0c
costapasta <nicholasmcosta@gmail.com>
2024-05-14 16:10:06 -0400

    fixed google translate feature, works on mobile and works to revert back to english (default) instead of translating to english (translation of translation)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,6 @@ import { Link } from 'react-scroll';
 import '../components-css/Header.css';
 import { ScrollProvider } from './ScrollContext';
 import useScrollDirection from './useScrollDirection';
-import useGoogleTranslate from './useGoogleTranslate';
 
 import usaIcon from './locations/icons/usa.png';
 import spainIcon from './locations/icons/spain.png';
@@ -16,14 +15,17 @@ function Header() {
   const lastScrollY = useRef(window.scrollY);
   const scrollDirection = useScrollDirection();
   const [linkClicked, setLinkClicked] = useState(false);
-  const isGoogleTranslateInitialized = useGoogleTranslate();
 
+
+  //Toggling menu
   const toggleMenu = () => {
     if (window.innerWidth <= 768) {
       setMenuOpen(!menuOpen);
     }
   };
 
+
+  // Handling navigation clicks to hide header
   const handleNavClick = () => {
     if (window.innerWidth <= 768) {
       toggleMenu();
@@ -35,6 +37,8 @@ function Header() {
     }, 1000);
   };
 
+
+  // Handles scrolling feature when clicking on nav links
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
@@ -52,11 +56,29 @@ function Header() {
     };
   }, [scrollDirection, linkClicked]);
 
+
+  // Function to change language through Header banner using Google Translate
   const changeLanguage = (langCode) => {
     const tryChangeLanguage = (retryCount = 0) => {
       const select = document.querySelector('select.goog-te-combo');
       console.log('changeLanguage called', langCode, select);
+      
       if (select) {
+        if (langCode === 'en') {
+          var restored = false;
+          Object.keys(window.googleTranslator).forEach((k) => {
+              if (restored) {
+                  return;
+              }
+              if (typeof window.googleTranslator[k]?.restore === 'function') {
+                  window.googleTranslator[k].restore();
+                  restored = true;
+              }    
+          });
+
+          // Also, flush the cookie that preserves the lang change across reloads
+          document.cookie="googtrans="
+        }
         select.value = langCode;
         select.dispatchEvent(new Event('change'));
       } else if (retryCount < 5) {
@@ -70,6 +92,8 @@ function Header() {
     tryChangeLanguage();
   };
 
+  
+  // Toggling the menu to hide when clicking on logo when viewed on mobile 
   const conditionalToggleMenu = () => {
     if (menuOpen) {
       toggleMenu();
