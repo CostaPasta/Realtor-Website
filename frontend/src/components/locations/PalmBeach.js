fe3e19089a220345d5711a60518af2e9d9e769a5
costapasta <nicholasmcosta@gmail.com>
2024-05-14 13:52:58 -0400

    added the .env for host name back

--- frontend/src/components/locations/PalmBeach.js
+++ frontend/src/components/locations/PalmBeach.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link as LinkScroll, } from "react-scroll";
 import Footer from "../Footer";
 import BackHeader from "../BackHeader";
