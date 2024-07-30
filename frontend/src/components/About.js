554cd64c455cc791836252f77cf6e8dd7d21bbf8
CostaPasta <nicholasmcosta@gmail.com>
2024-07-30 15:40:10 -0400

    adjusted the about me section, will need further adjustment, this is the base

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -9,25 +9,31 @@ const AboutMe = () => {
                 <img src={DadImageTransparent} alt="Jose Costa" />
             </div>
             <div className="profile-details">
-                <h2 id='name'>Looking for your dream home in South Florida?</h2>
-                <p>Navigating the South Florida real estate market can be challenging, but Jose Costa makes it simple.</p>
-
-                <h3 className='aboutMeSubTitle'>Local Expert:</h3>
+                <h2 id='name'>Meet <span className='highlight-blue'>Jose Costa</span>,</h2>
+                <h2 id='name-2'>Your South Florida <span className='highlight-orange'>Real Estate Expert</span></h2>
                 <p>
-                    Jose, has been a <u>South Florida resident</u> for over 30 YEARS with over 11 years of <u>real estate experience</u>, he's a trusted expert who understands the unique landscape.
+                    With over <span className='highlight-blue' style={{fontWeight: 'bold'}}>30 years</span> as a South Florida resident and <span className='highlight-blue' style={{fontWeight: 'bold'}}>11 years of real estate experience</span>, Jose Costa offers unparalleled local knowledge and expertise.
                 </p>
 
-                <h3 className='aboutMeSubTitle'>Multilingual Communication:</h3>
+                <h3 className='aboutMeSubTitle'>Why Choose Jose?</h3>
+                <p>
+                    Jose provides personalized guidance, ensuring you find your perfect home or investment. Fluent in <span style={{fontWeight: 'bold'}}>English, Spanish, and Portuguese</span>, he communicates effectively with diverse clients.
+                </p>
+                <p>
+                    Ready to start your journey? Contact Jose for a seamless and informed real estate experience.
+                </p>
+
+                {/* <h3 className='aboutMeSubTitle'>Multilingual Communication:</h3>
                 <p>
                     Fluent in <i>English, Spanish, and Portuguese</i>, Jose ensures seamless communication for both buyers and sellers, delivering top-notch service with genuine care.
                 </p>
 
                 <p>
                     Jose will guide you with detailed market insights and a deep understanding of your needs, ensuring you always get the best deal.
-                </p>
+                </p> */}
                 <div className="contact-details">
-                    <a className='notranslate' href="mailto:usarealtor1@gmail.com">JOSE@USArealtor.com</a>
-                    <a href="tel:+19546141351"> +1(954) 614-1351</a> 
+                    <a id='contact-info' className='notranslate' href="mailto:usarealtor1@gmail.com">JOSE@USArealtor.com</a>
+                    <a id='contact-info' href="tel:+19546141351">+1(954) 614-1351</a> 
                 </div>
             </div>
         </div>
