705fe5ba7f2d20f96a36db615831e62aa47a0096
CostaPasta <nicholasmcosta@gmail.com>
2024-08-14 10:39:53 -0400

    moved third party code to local, removed href tag

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -150,16 +150,16 @@ function Header() {
                 <img src={brazilIcon} alt="Portuguese" />
               </div>
             </div>
-            <Link to="aboutMeComponent" smooth={true} duration={600} offset={window.innerWidth <= 768 ? -35 : -38} onClick={handleNavClick} href='#aboutMeComponent'>
+            <Link to="aboutMeComponent" smooth={true} duration={600} offset={window.innerWidth <= 768 ? -35 : -38} onClick={handleNavClick}>
               ABOUT ME
             </Link>
-            <Link to="areasComponent" smooth={true} duration={600} offset={window.innerWidth <= 768 ? -35 : -38} onClick={handleNavClick} href='#areasComponent'>
+            <Link to="areasComponent" smooth={true} duration={600} offset={window.innerWidth <= 768 ? -35 : -38} onClick={handleNavClick}>
               AREAS
             </Link>
-            <Link to="searchComponent" smooth={true} duration={600} offset={window.innerWidth <= 768 ? -35 : -38} onClick={handleNavClick} href='#searchComponent'>
+            <Link to="searchComponent" smooth={true} duration={600} offset={window.innerWidth <= 768 ? -35 : -38} onClick={handleNavClick}>
               SEARCH
             </Link>
-            <Link to="contactComponent" smooth={true} duration={600} offset={window.innerWidth <= 768 ? -35 : -38} onClick={handleNavClick} href='#contactComponent'>
+            <Link to="contactComponent" smooth={true} duration={600} offset={window.innerWidth <= 768 ? -35 : -38} onClick={handleNavClick}>
               CONTACT
             </Link>
           </nav>
